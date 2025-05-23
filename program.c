d1fc9a51cf903b94c7a6d554af53fef95f8290e5
Filip5953 <zivkovicfilip264@gmail.com>
2025-05-23 10:30:19 +0200

    Update program.c
    izmenio sam prosli fajl

--- program.c
+++ program.c
@@ -3,5 +3,8 @@
 int main()
 {
   printf("Dobro dosli na kurs programiranja u C-u");
-  return 0;
+  printf("Dobro dosli na kurs programiranja u C-u");
+  printf("Dobro dosli na kurs programiranja u C-u");
+  printf("Dobro dosli na kurs programiranja u C-u");
+  printf("Dobro dosli na kurs programiranja u C-u");
 }  
